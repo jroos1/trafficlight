1fb4122711433c1a7f6b934d0c84623dae08f25b
Jason Roos <jason@disrupto.com>
2015-06-30 21:05:58 -0400

    Added break button to illuminate all lights

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document.getElementById('stopButton').onclick = illuminateRed;
 document.getElementById('slowButton').onclick = illuminateYellow;
 document.getElementById('goButton').onclick = illuminateGreen;
+document.getElementById('breakButton').onclick = illuminateAll;
 
 function illuminateRed() {
   clearLights();
@@ -14,6 +15,13 @@ function illuminateYellow() {
 
 function illuminateGreen() {
   clearLights();
+  document.getElementById('goLight').style.backgroundColor = "green";
+}
+
+function illuminateAll() {
+  clearLights();
+  document.getElementById('stopLight').style.backgroundColor = "red";
+  document.getElementById('slowLight').style.backgroundColor = "yellow";
   document.getElementById('goLight').style.backgroundColor = "green";
 }
 
